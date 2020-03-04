7541254da511fe729f92a9c826a972df5d688622
Eli Kittross-Schnell <eliks92@gmail.com>
2020-03-04 13:58:13 -0800

    have distilled top 50 tracks down into average audio features I have chosen.

--- visualize-api/api.go
+++ visualize-api/api.go
@@ -2,7 +2,22 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
+
+var db *gorm.DB
+
+func init() {
+	//open a db connection
+	var err error
+	db, err = gorm.Open("postgres", "host=myhost port=myport user=gorm dbname=gorm password=mypassword")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	//Migrate the schema
+	db.AutoMigrate(&tracksModel{})
+}
 
 func main() {
 	router := gin.Default()
@@ -10,7 +25,7 @@ func main() {
 	{
 		v1.POST("/", createTopTracks)
 		v1.GET("/", fetchTracks)
-		v1.DELETE("/:id", deleteTracks)
+		v1.DELETE("/", deleteTracks)
 	}
 	router.Run()
 }
--- visualize/src/components/Splash.js
+++ visualize/src/components/Splash.js
@@ -22,6 +22,33 @@ const hash = window.location.hash
   }, {});
 window.location.hash = "";
 
+const averageAudio = (audioArr) => {
+  let temp = {
+  danceability: 0,
+  energy: 0,
+  speechiness: 0,
+  acousticness: 0,
+  instrumentalness: 0,
+  liveness: 0,
+  valence: 0
+  }
+
+  for (const song of audioArr) {
+    temp.danceability += song.danceability
+    temp.energy += song.energy
+    temp.speechiness += song.speechiness
+    temp.acousticness += song.acousticness
+    temp.instrumentalness += song.instrumentalness
+    temp.liveness += song.liveness
+    temp.valence += song.valence
+  }
+
+  for (const key in temp) {
+    temp[key] = temp[key] / audioArr.length
+  }
+  return temp
+}
+
 export default function Splash() {
   const [token, setToken] = useState({});
 
@@ -39,6 +66,7 @@ export default function Splash() {
 
   useEffect(() => {
     console.log(Object.keys(token))
+    let averagesObj = {}
     let authString
     if(token !== {}) {
     authString = `Bearer ${token.token}`
@@ -52,7 +80,26 @@ export default function Splash() {
       } 
     })
     .then(function (response) {
-      console.log(response.data);
+      console.log(response)
+      let idString = ""
+      for(const song of response.data.items) {
+        idString += song['id'] + ","
+      }
+      const parsedIds = idString.substring(0, idString.length - 1);
+      axios({
+        method: 'get',
+        url: 'https://api.spotify.com/v1/audio-features/?ids=' + parsedIds,
+        dataType: 'json',
+        headers: { 
+          'Authorization': authString,
+        } 
+      })
+      .then(function (response) {
+        console.log(response.data.audio_features)
+      averagesObj = averageAudio(response.data.audio_features)
+      console.log(averagesObj)
+      })
+      
     })
     .catch(function (error) {
       console.log(error.response, "error");
